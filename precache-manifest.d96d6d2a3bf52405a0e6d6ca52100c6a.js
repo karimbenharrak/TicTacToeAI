5962d88f9eb3c3993eaef24e08e34cd9545f4c00
DerKarim06 <karimbenharrak@icloud.com>
2020-09-30 10:57:47 +0200

    Updates

---
+++ precache-manifest.d96d6d2a3bf52405a0e6d6ca52100c6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af59b8aa82683aefd22b8f487f124da5",
+    "url": "/TicTacToeAI/index.html"
+  },
+  {
+    "revision": "ddb24e7b397681e6234f",
+    "url": "/TicTacToeAI/static/css/main.f782d95f.chunk.css"
+  },
+  {
+    "revision": "5d8d7c88885f46632654",
+    "url": "/TicTacToeAI/static/js/2.9999ca9d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TicTacToeAI/static/js/2.9999ca9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddb24e7b397681e6234f",
+    "url": "/TicTacToeAI/static/js/main.6dd6f7f7.chunk.js"
+  },
+  {
+    "revision": "2a5a69d6915748618ecb",
+    "url": "/TicTacToeAI/static/js/runtime-main.c3813b22.js"
+  }
+]);
